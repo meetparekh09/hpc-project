9e735f4831284a88fe74f582ec4558513147fe7c
meetparekh09 <meetparekh09@gmail.com>
2019-05-03 14:38:34 -0400

    Stochastic Gradient Descent

--- sgd.cpp
+++ sgd.cpp
@@ -8,7 +8,47 @@ void gradient(double *x, double *y, double *theta, double (*h)(double *x, double
 double hypothesis(double *x, double *theta, int n);
 void gradient_descent(double *x, double *y, double *theta, double (*h)(double *x, double *theta, int n),
 int n, int m, int num_iters);
+void stochastic_gradient_descent(double *x, double *y, double *theta, double (*h)(double *x, double *theta, int n),
+int n, int m, int num_iters);
+void gradient_update(double *x, double *y, double *theta, double (*h)(double *x, double *theta, int n),
+ double *cost, int n, int m, double alpha);
 
+
+void stochastic_gradient_descent(double *x, double *y, double *theta, double (*h)(double *x, double *theta, int n),
+int n, int m, int num_iters) {
+    double alpha = 1.0/n;
+    double cost = 0.0;
+
+    for(int i = 0; i < num_iters; i++) {
+        gradient_update(x, y, theta, &hypothesis, &cost, n, m, alpha);
+        if(i % 100 == 0)
+            printf("Iter :: %d, Cost :: %f\n", i, cost);
+        cost = 0.0;
+    }
+
+}
+
+
+void gradient_update(double *x, double *y, double *theta, double (*h)(double *x, double *theta, int n),
+ double *cost, int n, int m, double alpha) {
+
+     for(int i = 0; i < m*n; i+=n) {
+         double h_val = h(x+i, theta, n);
+         double diff = y[i/n] - h_val;
+         *cost += diff*diff;
+     }
+
+    for(int i = 0; i < m*n; i+=n) {
+        double h_val = h(x+i, theta, n);
+        double diff = y[i/n] - h_val;
+
+        for(int j = i; j < i + n; j++) {
+            theta[j%n] += alpha*diff*x[j];
+        }
+    }
+
+    *cost = (*cost)/2;
+}
 
 
 void gradient_descent(double *x, double *y, double *theta, double (*h)(double *x, double *theta, int n),
@@ -23,7 +63,9 @@ int n, int m, int num_iters) {
 
     for(int i = 0; i < num_iters; i++) {
         gradient(x, y, theta, &hypothesis, &cost, grad, n, m);
-        printf("Iter :: %d, Cost :: %f\n", i, cost);
+
+        if(i % 100 == 0)
+            printf("Iter :: %d, Cost :: %f\n", i, cost);
         for(int j = 0; j < n; j++) {
             theta[j] += alpha*grad[j];
             grad[j] = 0.0;
@@ -59,8 +101,8 @@ double hypothesis(double *x, double *theta, int n) {
 }
 
 int main() {
-    int n = 100;
-    int m = 10000;
+    int n = 1000;
+    int m = 1000000;
     int num_iters = 1000;
 
     double *x = (double*)malloc(m*n*sizeof(double));
@@ -75,7 +117,25 @@ int main() {
         }
     }
 
+
+    printf("==============================================================================================\n");
+    printf("Convergence in Gradient Descent :: \n\n\n");
     gradient_descent(x, y, theta, hypothesis, n, m, num_iters);
+    printf("\n\n\n");
 
 
+    for(int i = 0; i < m*n; i+=n) {
+        y[i/n] = drand48();
+        for(int j = i; j < i + n; j++) {
+            x[j] = drand48();
+            if(i == 0) theta[j] = drand48();
+        }
+    }
+
+
+    printf("==============================================================================================\n");
+    printf("Convergence in Stochastic Gradient Descent :: \n\n\n");
+    stochastic_gradient_descent(x, y, theta, hypothesis, n, m, num_iters);
+    printf("\n\n\n");
+    printf("==============================================================================================\n");
 }
